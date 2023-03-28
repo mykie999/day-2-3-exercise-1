e610cd66a7758450fa5240102b0422c392acde39
myki3 Michael <90443872+mykie999@users.noreply.github.com>
2023-03-28 14:50:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+real_age = int(age)
+years_remaining = 90 - real_age
+days = years_remaining * 365
+weeks = years_remaining * 52
+months = years_remaining * 12
+
+
+
+
+print(f"You have {days} days, {weeks} weeks, {months} months left." )
+
+
+
+
+
+
